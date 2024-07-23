5aad2ac1ee22c550c444c08378ceaa2315aec27e
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-07-23 08:10:43 -1000

    Add files via upload

---
+++ Marginis/FranklinMcNicholl2020.lean
@@ -0,0 +1,52 @@
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Topology.MetricSpace.Basic
+
+/-
+
+Franklin and McNicholl define a metric on a graph by:
+d_G(v₀,v₁) =
+0 if v₀ = v₁
+1 if (v₀,v₁) ∈ E;
+2 otherwise
+
+They say this is "clearly" a metric. We prove this formally and
+generalize it, by replacing 1 and 2 by real numbers
+0 < a ≤ 2b, b ≤ 2a (the fact that 0 < b follows but does not need to be mentioned).
+
+-/
+
+open Classical
+noncomputable instance {U : Type}  (G : SimpleGraph U) (a b : ℝ)
+(h₀ : 0 < a) (h₁ : a ≤ b + b) (h₂ : b ≤ a + a)
+: MetricSpace U := {
+  dist := λ x y ↦ ite (x=y) 0 (ite (G.Adj x y) a b)
+  dist_self := λ x ↦ by simp only [reduceIte]
+  dist_comm := λ x y ↦ by
+    unfold dist
+    simp
+    by_cases H : x = y
+    . rw [if_pos H, if_pos H.symm]
+    . rw [if_neg H]
+      have hne: ¬ y = x := fun a ↦ H (id (Eq.symm a))
+      by_cases H' : G.Adj x y
+      . rw [if_pos H', if_neg hne]
+        have : G.Adj y x := SimpleGraph.adj_symm G H'
+        rw [if_pos this]
+      . rw [if_neg H', if_neg hne]
+        have : ¬ G.Adj y x := fun a ↦ H' (SimpleGraph.adj_symm G a)
+        rw [if_neg this]
+  dist_triangle := λ x y z ↦ by
+    aesop; repeat linarith
+  edist_dist := (λ x y ↦ by exact (ENNReal.ofReal_eq_coe_nnreal _).symm)
+  eq_of_dist_eq_zero := by
+    intro x y h
+    simp at h
+    contrapose h
+    push_neg
+    use h
+    by_cases H : G.Adj x y
+    rw [if_pos H]
+    linarith
+    rw [if_neg H]
+    linarith
+}
---
+++ Marginis/NeriPowell2023.lean
@@ -0,0 +1,55 @@
+import Mathlib.Topology.MetricSpace.PiNat
+import Mathlib.Data.Real.Basic
+
+/-
+This is the first example from
+
+A computational study of a class of recursive inequalities
+MORENIKEJI NERI
+THOMAS POWELL
+-/
+
+example (μ : ℕ → NNReal) (c : Set.Ico (0:ℝ) 1)
+  (h : ∀ n, μ (n + 1) ≤ c.1 * μ n) (n : ℕ):
+  μ n ≤ c.1^n * μ 0 := by
+  induction n with
+  |zero => simp only [pow_zero, one_mul, le_refl]
+  |succ n hn =>
+    calc
+    _ ≤ c.1 * μ n := h n
+    _ ≤ _ := by
+      ring_nf;
+      rw [mul_assoc]
+      exact mul_le_mul_of_nonneg_left hn c.2.1
+
+/-
+For the inequality to hold we do not need to use the type
+`NNReal` but can also use `Real`; and we can generalize `c` as well:
+-/
+
+example (μ : ℕ → Real) (c : NNReal)
+  (h : ∀ n, μ (n + 1) ≤ c * μ n) (n : ℕ):
+  μ n ≤ c^n * μ 0 := by
+  induction n with
+  |zero => simp only [pow_zero, one_mul, le_refl]
+  |succ n hn =>
+    calc
+    _ ≤ c * μ n := h n
+    _ ≤ _ := by
+      ring_nf;
+      rw [mul_assoc]
+      exact mul_le_mul_of_nonneg_left hn c.2
+
+/- Using rationals and nonnegative rationals works with the same proof: -/
+example (μ : ℕ → Rat) (c : NNRat)
+  (h : ∀ n, μ (n + 1) ≤ c * μ n) (n : ℕ):
+  μ n ≤ c^n * μ 0 := by
+  induction n with
+  |zero => simp only [pow_zero, one_mul, le_refl]
+  |succ n hn =>
+    calc
+    _ ≤ c * μ n := h n
+    _ ≤ _ := by
+      ring_nf;
+      rw [mul_assoc]
+      exact mul_le_mul_of_nonneg_left hn c.2
