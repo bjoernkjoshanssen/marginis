4941ea934b366e456e2a0567d53580bc00f6e244
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-10-09 06:34:13 -1000

    adding use of kolmogorovextension4

--- Marginis/CalvertFranklin2015.lean
+++ Marginis/CalvertFranklin2015.lean
@@ -5,24 +5,30 @@ import Mathlib.Order.Interval.Set.Basic
 import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.RingTheory.Regular.RegularSequence
 
-/- This concerns the second sentence in the Introduction in the paper
-Genericity and UD–random reals by WESLEY CALVERT, JOHANNA N.Y. FRANKLIN.
+/-!
+## Genericity and UD–random reals
+by
+# WESLEY CALVERT, JOHANNA N.Y. FRANKLIN.
+
+This concerns the second sentence in the Introduction in the paper.
+
 We define uniform distribution and prove that the constant 0 sequence is not uniformly distributed.
 -/
 
 open Finset
 
-variable (x : ℕ → Set.Ico (0:ℝ) 1)
-variable (n:ℕ)
-variable (i : Fin n)
-variable (a b:ℝ)
-
-def uniformly_distributed (x : ℕ → Set.Ico (0:ℝ) 1) :=
-  ∀ a b ε : ℝ, 0 ≤ a → a < b → b ≤ 1 → ε > 0 → ∃ n₀ : ℕ, ∀ n : ℕ, n ≥ n₀ →
+/-- The triple (a,b,ε) satisfies the conditions for uniform distribution of x. -/
+def uniformly_distributed_at (x : ℕ → Set.Ico (0:ℝ) 1) (a b ε : ℝ) :=
+  ∃ n₀ : ℕ, ∀ n : ℕ, n ≥ n₀ →
     abs (card (filter (λ i : Fin n ↦ a < x i ∧ x i < b) univ) - (b - a) * n) < n * ε
 
-example : ¬ uniformly_distributed (λ _ ↦ ⟨0,by simp⟩) := by
-  unfold uniformly_distributed
+/-- The sequence `x` is uniformly distributed in the half-open unit interval. -/
+def uniformly_distributed (x : ℕ → Set.Ico (0:ℝ) 1) :=
+  ∀ a b ε : ℝ, 0 ≤ a → a < b → b ≤ 1 → ε > 0 → uniformly_distributed_at x a b ε
+
+/-- The constant zero sequence is not uniformly distributed. -/
+lemma zero_not_uniformly_distributed : ¬ uniformly_distributed (λ _ ↦ ⟨0,by simp⟩) := by
+  unfold uniformly_distributed uniformly_distributed_at
   push_neg
   use (1:ℝ)/2;use (1:ℝ);use (1:ℝ)/2
   constructor;aesop;constructor;exact one_half_lt_one
--- Marginis/MiyabeNiesStephan2018.lean
+++ Marginis/MiyabeNiesStephan2018.lean
@@ -1,5 +1,8 @@
 import Mathlib.MeasureTheory.Measure.Hausdorff
 import Marginis.Pathak2009
+import Marginis.KolmogorovExtension4.ProductMeasure
+import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
+import Mathlib.Probability.ProbabilityMassFunction.Basic
 /-
 
 Miyabe, Nies, Stephan, `Randomness and Solovay degrees`, JLA, 2018, page 3 says:
@@ -12,6 +15,9 @@ Here we prove the well known fact that this representation is not unique.
 We also show that consequently, if we use `Mathlib`'s current definition of
 the pullback measure as of June 23, 2024 as a basis for a measure on Cantor space,
 the Cantor space gets measure 0.
+
+However, if we use Etienne Marion's KolmogorovExtension4 library
+thinks work out well.
  -/
 
 lemma geom_value : ∑' (n : ℕ), ((1:ℝ) / 2 ^ n.succ)  = 1 := by
@@ -24,7 +30,7 @@ lemma geom_summable: Summable (λ n : ℕ ↦ (1:ℝ) / 2^n.succ) := by
             apply funext;intro x;ring_nf
       let T := @summable_mul_right_iff ℕ ℝ _ _ _ (λ x ↦ (1 / 2)^x) (1/2) (by simp)
       rw [this, T]
-      exact @NormedRing.summable_geometric_of_norm_lt_one
+      exact @summable_geometric_of_norm_lt_one
                 ℝ _ _ (1/2) (by simp; exact two_inv_lt_one)
 
 
@@ -83,3 +89,132 @@ lemma because_real_of_cantor_not_injective : CantorLebesgueMeasure₀ Set.univ =
   . contrapose H
     simp
     simp at H
+
+open Classical
+
+noncomputable instance p : PMF Bool := {
+  val := fun b => (1:ENNReal)/2
+  property := by
+    have h₀ :=  @hasSum_fintype ENNReal Bool _ _ _ (fun b => 1/2)
+    have h₁ : ∑ b : Bool, (1:ENNReal)/2 = 1 := by
+      simp
+      field_simp
+      refine (ENNReal.div_eq_one_iff ?hb₀ ?hb₁).mpr rfl
+      simp
+      simp
+    aesop
+}
+
+noncomputable def β : MeasureTheory.ProbabilityMeasure Bool := {
+  val := p.toMeasure
+  property := PMF.toMeasure.isProbabilityMeasure p
+}
+
+instance (n : ℕ) : MeasureTheory.IsProbabilityMeasure ((fun _ ↦ β.val) n) := by
+  simp
+  exact β.property
+
+example : @MeasureTheory.Measure.infinitePiNat (fun _ : ℕ => Bool) _
+    (fun _ => β.val) (fun _ => β.property)
+    Set.univ = 1 := by
+    simp only [
+      MeasureTheory.ProbabilityMeasure.val_eq_to_measure,
+      MeasureTheory.measure_univ
+    ]
+
+lemma l₀ (b : Bool) : p b = 1/2 := by
+  unfold p
+  show (fun b : Bool => (1:ENNReal)/2) true = 1/2
+  simp
+
+lemma l₁ (b : Bool) : β {b} = 1/2 := by
+  unfold β
+  simp
+  have := l₀ b
+  cases b
+  simp_all
+  rfl
+  simp_all
+  rfl
+
+noncomputable def μ := @MeasureTheory.productMeasure Nat (fun _ => Bool)
+    _ (fun _ => β) _
+
+instance : MeasureTheory.IsProbabilityMeasure μ := by
+  refine MeasureTheory.isProbabilityMeasure_iff.mpr ?_
+  unfold μ
+  exact MeasureTheory.measure_univ
+
+lemma l₅: μ Set.univ = 1 := MeasureTheory.measure_univ
+
+lemma l₄ (b : Bool) (k : ℕ) : μ {A | A k = b} = 1/2 := by
+      have h₀ := @MeasureTheory.productMeasure_boxes ℕ (fun _ => Bool) _
+        (fun _ => β) _ {k} (fun i => {b}) (by simp)
+      simp_all
+      unfold Function.eval at h₀
+      have h₂ : {f : ℕ → Bool | f k = b} = ((fun f ↦ f k) ⁻¹' {b}) := by aesop
+      rw [← h₂] at h₀
+      unfold μ
+      rw [h₀]
+      have nnreal := l₁ b
+      simp_all
+      clear h₀
+      clear h₂
+      have : (β {b} : NNReal) = (2⁻¹ : NNReal) := nnreal
+      have g₀ : (β {b} : ENNReal) = (2⁻¹ : ENNReal) := by aesop
+      rw [← g₀]
+      simp
+
+
+/-- Oct 8, 2024 -/
+lemma l₆ {s : ℕ} (b : Fin s → Bool) : μ {A | ∀ k : Fin s, A k.1 = b k} = (1/2)^s := by
+  have h₀ := @MeasureTheory.productMeasure_boxes ℕ (fun _ => Bool) _
+    (fun _ => β) _ {k < s | true}
+    (fun k => dite (k < s) (fun h => {b ⟨k,h⟩}) (fun _ => Set.univ))
+    (by simp)
+  unfold μ
+  have g₀ : (MeasureTheory.productMeasure fun x ↦ β.toMeasure)
+    ((Set.Iio s).pi fun k ↦ if h : k < s then {b ⟨k, h⟩} else {false, true})
+          = (MeasureTheory.productMeasure fun x ↦ β.toMeasure)
+    {A | ∀ (k : Fin s), A k.1 = b k} := by
+    congr
+    ext A
+    simp
+    constructor
+    aesop
+    intro h
+    intro i hi
+    have := h ⟨i,hi⟩
+    simp_all
+
+  have g₁ : ∏ x ∈ Finset.Iio s, β.toMeasure
+    (if h : x < s then {b ⟨x, h⟩} else {false, true}) = ((1:ENNReal)/2)^s := by
+    have g₂: ∀  x ∈ Finset.Iio s, β.toMeasure
+      (if h : x < s then {b ⟨x, h⟩} else {false, true})
+      = 1/2 := by
+      intro x hx;
+      split_ifs with j₀
+      congr
+      have := l₁ <|b ⟨x, j₀⟩
+      clear g₀ h₀ hx
+      unfold β
+      aesop
+      cases b ⟨x, j₀⟩
+      aesop
+      rw [l₀]
+      aesop
+      simp
+      rw [l₀]
+      simp
+      unfold β
+      aesop
+    have g₃ : ∏ x ∈ Finset.Iio s, ((1:ENNReal)/2) = (1/2)^s := by
+      have := @Finset.prod_const ℕ ENNReal (Finset.Iio s) _ (1/2)
+      rw [this]
+      congr
+      exact Nat.card_Iio s
+    rw [← g₃]
+    exact Finset.prod_congr rfl g₂
+  rw [← g₀]
+  rw [← g₁]
+  simp_all
