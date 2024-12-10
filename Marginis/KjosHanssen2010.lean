7c2681d90a16364425f0c6c676d1f23b5cb62f15
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-12-10 08:52:18 -1000

    Clean up Kjos-Hanssen 2010

--- Marginis/KjosHanssen2010.lean
+++ Marginis/KjosHanssen2010.lean
@@ -1,15 +1,20 @@
 import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
+/-!
+# Permutations of the integers induce only the trivial automorphism of the Turing degrees
+by Kjos-Hanssen, Bjørn
 
+https://arxiv.org/pdf/1408.2850.pdf
 
-theorem page5_display3_equals2 (b:ℝ): (2:ℝ)^(b+1) * 4^(b-1) = 2^(3*b-1):=
-/-
-Formal verification of
+-/
+
+/--
 https://arxiv.org/pdf/1408.2850.pdf
 Page 5
 3rd displayed equation
 Second "equals" sign
 -/
+theorem page5_display3_equals2 (b:ℝ): (2:ℝ)^(b+1) * 4^(b-1) = 2^(3*b-1):=
   (calc
   2^(3*b-1) = 2^(b+1 + 2*(b-1))     := by ring_nf
   _         = 2^(b+1) * 2^(2*(b-1)) := Real.rpow_add zero_lt_two _ _
@@ -17,15 +22,14 @@ Second "equals" sign
   _         = 2^(b+1) * 4^(b-1)     := by norm_num).symm
 
 
-theorem page7_display4_lt2 (b c : ℝ) (h: b < c) :
-(2:ℝ)^(-b) + 2^(-c) < 2^(-(b-1)) :=
-/-
-Formal verification of
+/--
 https://arxiv.org/pdf/1408.2850.pdf
 Page 7
 4th displayed equation
 Second "<" sign
 -/
+theorem page7_display4_lt2 (b c : ℝ) (h: b < c) :
+(2:ℝ)^(-b) + 2^(-c) < 2^(-(b-1)) :=
   have : -c < -b         := neg_lt_neg h
   have : (2:ℝ)^(-c) < 2^(-b) := (Real.rpow_lt_rpow_left_iff one_lt_two).mpr this
   calc
@@ -38,12 +42,6 @@ Second "<" sign
     simp only [pow_one, Real.rpow_one]
   _ = 2^(-(b-1))      := by ring_nf
 
-/-
-Page 8
-5th displayed equation
-Inequality
-proved in paper_bound'' below
--/
 
 theorem bound_of_nonneg (p:ℝ) : 0 ≤ 4*p*(p-1) + 1 := calc
   _ ≤ (2*p-1)*(2*p-1) := mul_self_nonneg _
@@ -57,8 +55,9 @@ theorem numerator_bound (p:ℝ) : 4*(p*(1-p)) ≤ 1 :=
   _ ≤ -0 + 1                := add_le_add_right (neg_le_neg (bound_of_nonneg p)) 1
   _ = 1                     := by ring
 
-theorem le_div {a b c : ℝ} (ha: 0<a) (g: a*b ≤ c) : b ≤ c/a :=
-  ((le_div_iff' ha).mpr) g
+theorem le_div {a b c : ℝ} (ha: 0<a) (g: a*b ≤ c) : b ≤ c/a := by
+  apply (le_div_iff₀ ha).mpr
+  linarith
 
 theorem the_bound (p:ℝ) : p*(1-p) ≤ 1/4 :=
   le_div zero_lt_four (numerator_bound p)
@@ -104,9 +103,14 @@ _ ≤ p * (1-p) * 2 := mul_le_mul_of_nonneg_left (paper_bound h0 h1) (mul_nonneg
 _ ≤ (1/4) * 2     := mul_le_mul_of_nonneg_right (the_bound _) (by linarith)
 _ = _             := by ring_nf
 
-example : (0:ℝ)^(0:ℝ) = 1 := by exact Real.rpow_zero 0
-example : (0)^(0:ℕ) = 1 := by exact rfl
+-- example : (0:ℝ)^(0:ℝ) = 1 := by exact Real.rpow_zero 0
+-- example : (0)^(0:ℕ) = 1 := by exact rfl
 
+/--
+Page 8
+5th displayed equation
+Inequality
+-/
 theorem paper_bound'' {p : ℝ} (h0 : 0 ≤ p) (h1 : p ≤ 1) :
   (1-p)^(4:ℝ) * p + p^(4:ℝ) * (1-p) ≤ 1/2 :=
 calc
@@ -137,8 +141,8 @@ calc
 
 /- Bottom of page 7: -/
 /- Caution is needed since 3/2=1 and 3*(1/2)=0 over ℕ. -/
-#eval 3/2
-#eval 3*(1/2)
+-- #eval 3/2
+-- #eval 3*(1/2)
 
 open Nat
 theorem choose_two (n:ℕ) : 2 * choose n 2  = n*(n-1) := match n with
